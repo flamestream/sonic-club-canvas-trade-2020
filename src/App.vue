1d480cc0a5b7ec3052ffaf01017b0eb84a2918e7
Nicholas Souphandavong <flamestream@gmail.com>
2020-06-09 01:56:24 -0400

    First version.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,172 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <v-container
+        fluid
+      >
+        <v-row>
+          <v-col>
+            <div align="center" class="mb-2">
+              <v-img
+                :src="require('./assets/banner.png')"
+                width="80%"
+              />
+              <div class="display-1">Sonic Club's Canvas Trading 2020 Event</div>
+              <div class="subtitle-1">Event in progress!</div>
+            </div>
+            <v-card>
+              <v-container fluid>
+                <v-row
+                  v-for="(stageGroup, stageGroupIdx) in stageGroups"
+                  :key="stageGroupIdx"
+                  justify="center"
+                  dense
+                >
+                  <v-col
+                    v-for="stageIndex in stageCount"
+                    :key="stageIndex"
+                    class="d-flex child-flex"
+                  >
+                    <Entry
+                      :previewPath="previewPathFromStage(stageIndex, stageGroup[stageIndex - 1])"
+                      :downloadPath="downloadPathFromStage(stageIndex, stageGroup[stageIndex - 1])"
+                      :avatarUrl="avatarUrlFromStage(stageGroup[stageIndex - 1])"
+                    />
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import Entry from './components/Entry';
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    Entry
   },
 
   data: () => ({
-    //
+    participants: {
+      '4sonicfan': {
+        gallery: null,
+        sns: null,
+      },
+      'ArmorGontier': {
+        gallery: null,
+        sns: null,
+      },
+      'AussieHobbyYeah': {
+        gallery: null,
+        sns: null,
+      },
+      'Clever The Wolf': {
+        gallery: null,
+        sns: null,
+      },
+      'Flamestream': {
+        gallery: null,
+        sns: null,
+      },
+      'Flappy': {
+        gallery: null,
+        sns: null,
+      },
+      'Goodie_Noodles (Nichan)': {
+        gallery: null,
+        sns: null,
+      },
+      'Mllermanda': {
+        gallery: null,
+        sns: null,
+      },
+      'Nini Rosé': {
+        gallery: null,
+        sns: null,
+      },
+      'NOOB': {
+        gallery: null,
+        sns: null,
+      },
+      'Rotten': {
+        gallery: null,
+        sns: null,
+      },
+      'TylerBucket': {
+        gallery: null,
+        sns: null,
+      },
+      'Wolfie': {
+        gallery: null,
+        sns: null,
+      },
+      '🌌💮𝓢𝐏ε𝓵ｌ 🔮ғ𝕋𝒽𝐞𝓓έ𝐚ｄ🌸🌆🌇🌺': {
+        gallery: null,
+        sns: null,
+      },
+    },
+    stageGroups: [
+      ['4sonicfan', 'Clever The Wolf'],
+      ['ArmorGontier', 'ArmorGontier'],
+      ['AussieHobbyYeah', 'NOOB'],
+      ['Clever The Wolf', 'Nini Rosé-2'],
+      ['Flamestream', 'Mllermanda'],
+      ['Flamestream-2', 'Wolfie'],
+      ['Flappy', '🌌💮𝓢𝐏ε𝓵ｌ 🔮ғ𝕋𝒽𝐞𝓓έ𝐚ｄ🌸🌆🌇🌺'],
+      ['Goodie_Noodles (Nichan)', 'Flappy'],
+      ['Goodie_Noodles (Nichan)-2', '🌌💮𝓢𝐏ε𝓵ｌ 🔮ғ𝕋𝒽𝐞𝓓έ𝐚ｄ🌸🌆🌇🌺-2'],
+      ['Mllermanda', 'Flamestream'],
+      ['Nini Rosé', 'AussieHobbyYeah'],
+      ['Nini Rosé-2', 'Flamestream-2'],
+      ['NOOB', 'TylerBucket'],
+      ['Rotten', 'Nini Rosé'],
+      ['TylerBucket', 'Goodie_Noodles (Nichan)'],
+      ['Wolfie', 'Rotten'],
+      ['🌌💮𝓢𝐏ε𝓵ｌ 🔮ғ𝕋𝒽𝐞𝓓έ𝐚ｄ🌸🌆🌇🌺', 'Goodie_Noodles (Nichan)-2'],
+      ['🌌💮𝓢𝐏ε𝓵ｌ 🔮ғ𝕋𝒽𝐞𝓓έ𝐚ｄ🌸🌆🌇🌺-2', '4sonicfan'],
+    ],
+    stageCount: 5,
   }),
+
+  computed: {
+    displayedIds() {
+      return this.maxSelected ? this.selectedIds : Object.keys(this.images);
+    },
+  },
+
+  methods: {
+    previewPathFromStage(stageIndex, stageName) {
+      if (!stageName) return;
+      return `/stage-${stageIndex}/${stageName}.png`
+    },
+    downloadPathFromStage(stageIndex, stageName) {
+      if (stageIndex > 1) return;
+      if (!stageName) return;
+      return `entries/stage-${stageIndex}/${stageName}.psd`
+    },
+    avatarUrlFromStage(stageName) {
+      if (!stageName) return;
+      console.log(stageName)
+      let participantName = stageName.match(/(.+?)(-\d+)?$/)[1];
+      console.log(`./assets/avatars/${participantName}.png`)
+      return require(`./assets/avatars/${participantName}.png`)
+    },
+  },
+
+  async created () {
+    this.$vuetify.theme.dark = true
+  },
 };
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+  transition: all 1s;
+}
+</style>
---
+++ src/components/Entry.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container class="pa-0">
+    <v-card flat tile class="d-flex">
+      <v-row
+        class="fill-height ma-0"
+        align="center"
+        justify="center"
+      >
+        <v-img
+          :src="previewPath && `entries/${previewPath}`"
+          aspect-ratio="1"
+          :contain="true"
+          background-color="grey darken-4"
+          class="img"
+        >
+          <v-row align="end" class="description white--text fill-height ma-0">
+            <v-col align="end">
+              <v-btn v-if="downloadPath" class="mr-2" fab x-small dark :href="downloadPath">
+                <v-icon>mdi-download</v-icon>
+              </v-btn>
+              <v-avatar color="blue-grey darken-4">
+                <img v-if="avatarUrl"
+                  :src="avatarUrl"
+                >
+                <span v-else class="white--text headline">?</span>
+              </v-avatar>
+            </v-col>
+          </v-row>
+          <template v-slot:placeholder>
+            <v-row class="fill-height" align="center">
+              <v-col align="center" class="font-weight-black">
+                NOW<br/>PRINTING
+              </v-col>
+            </v-row>
+          </template>
+        </v-img>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Entry',
+    props: ['previewPath', 'downloadPath', 'avatarUrl']
+  }
+</script>
+
+<style scoped>
+  .description {
+    /*box-shadow: 0 0 20px inset rgba(0, 0, 0, 0.2);*/
+    /*background: rgba(0, 0, 0, 0.8);*/
+  }
+  .img {
+    background: #212121;
+  }
+</style>
